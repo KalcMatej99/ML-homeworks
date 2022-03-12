4d45d69a32e798a8b8f41cb50264c7e0dd65c621
Matej Kalc <mk9693@student.uni-lj.si>
2022-03-12 16:00:06 +0100

    Added hw2

--- HW2/solution.py
+++ HW2/solution.py
@@ -1,6 +1,8 @@
-from dis import dis
 import numpy as np
 from scipy.optimize import fmin_l_bfgs_b
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import random
 
 
 class MultinomialLogReg:
@@ -8,6 +10,7 @@ class MultinomialLogReg:
         self.X = None
         self.y = None
         self.m = 0
+        self.best_log_score = 999999999999999999999
 
     def build(self, X, y):
         self.X = X
@@ -24,7 +27,6 @@ class MultinomialLogReg:
             [np.array([1] * (self.number_of_columns + 1))] * (m - 1)
         )
         B = fmin_l_bfgs_b(self.loglikelihood, initial_guess, approx_grad=True)
-        
 
         B = B[0]
         B = np.concatenate((B, [0] * (self.number_of_columns + 1)), axis=0)
@@ -32,8 +34,6 @@ class MultinomialLogReg:
         B = B.reshape((self.m, self.number_of_columns + 1))
 
         self.B = B
-
-        print(self.B)
 
         return self
 
@@ -54,26 +54,18 @@ class MultinomialLogReg:
         B = np.concatenate((B, [0] * (self.number_of_columns + 1)), axis=0)
         B = B.reshape((self.m, self.number_of_columns + 1))
 
-        """
-        loss = 0
-        for x_i, y_i in zip(self.X, self.y):
-            x_i_e = np.concatenate((x_i, [1]))
-            N = np.sum([np.exp(-np.dot(b, x_i_e)) for b in B])
-            loss += np.sum([np.log(np.exp(-np.dot(B[j], x_i_e))/N) for j in range(len(B)) if self.possible_outcomes[j] == y_i])
-        print("loss", -loss)
-        """
-
         values = -np.dot(self.X_e, np.transpose(B))
         exp_values = np.exp(values)
         Ns = np.sum(exp_values, axis=1)
-        #Ns = [[1 / N] * len(B) for N in Ns]
-        #log_exp_values = np.log(np.multiply(exp_values, Ns))
         mask = np.transpose(
             [self.y == self.possible_outcomes[j] for j in range(len(B))]
         )
         Ns[Ns == 0] += 0.000001
-        log_exp_values = exp_values[mask]/(Ns)
+        log_exp_values = exp_values[mask] / (Ns)
         loss = np.sum(np.log(log_exp_values))
+
+        if -loss < self.best_log_score:
+            self.best_log_score = -loss
         return -loss
 
 
@@ -83,6 +75,7 @@ class OrdinalLogReg:
         self.y = None
         self.m = 0
         self.t = []
+        self.best_log_score = 99999999999999999
 
     def build(self, X, y):
         self.X = X
@@ -101,14 +94,10 @@ class OrdinalLogReg:
         bounds += [(0, np.Inf)] * self.k
 
         output = fmin_l_bfgs_b(
-            self.loglikelihood,
-            initial_guess,
-            approx_grad=True,
-            bounds=bounds
+            self.loglikelihood, initial_guess, approx_grad=True, bounds=bounds
         )
         output = output[0]
         self.B = output[: self.number_of_columns + 1]
-        print(self.B)
 
         discplacments = output[self.number_of_columns + 1 :]
         c = 0
@@ -119,7 +108,6 @@ class OrdinalLogReg:
         self.t = [np.NINF, 0]
         self.t.extend(t_i)
         self.t.extend([np.Inf])
-        print(self.t)
 
         return self
 
@@ -160,26 +148,89 @@ class OrdinalLogReg:
         possible_y = list(np.unique(self.y))
 
         for x_i, y_i in zip(self.X, self.y):
-            prob_x_i = []
             x_i_e = np.concatenate((x_i, [1]))
             u_i = np.dot(B, x_i_e)
-            for j in range(1, len(t)):
-                if y_i == possible_y[j - 1]:
-                    prob_x_i.append(
-                        np.log(
-                            self.cdf_standard_logistic_d(t[j] - u_i)
-                            - self.cdf_standard_logistic_d(t[j - 1] - u_i)
-                        )
-                    )
-                else:
-                    prob_x_i.append(0)
-            loss += np.sum(prob_x_i)
-        #print("loss", -loss)
+            j = possible_y.index(y_i) + 1
+            loss += np.log(
+                self.cdf_standard_logistic_d(t[j] - u_i)
+                - self.cdf_standard_logistic_d(t[j - 1] - u_i)
+            )
+
+        if -loss < self.best_log_score:
+            self.best_log_score = -loss
         return -loss
 
 
 def multinomial_bad_ordinal_good(n, rand):
-    return [0] * n
+    X = np.array([[rand.uniform(0, 1) + i] for i in range(n)])
+    y = np.array(
+        [
+            int(x[0] <= n / 3)
+            + 2 * int(x[0] <= (2 * n) / 3 and x[0] > n / 3)
+            + 3 * int(x[0] > (2 * n) / 3)
+            for x in X
+        ]
+    )
+    y -= 1
+    return X, y
 
 
-MBOG_TRAIN = 100
+MBOG_TRAIN = 50
+
+if __name__ == "__main__":
+    
+    print("Part 2")
+    df = pd.read_csv("dataset.csv", delimiter=";")
+    X = df.loc[:, df.columns != 'ShotType']
+    y = df["ShotType"]
+    X = pd.get_dummies(X, columns = ['Competition', 'PlayerType', 'Movement'], drop_first=True)
+
+    possible_outcomes = np.unique(y.to_numpy())
+
+    def normalize(X_train, column):
+        u = np.mean(X_train[column])
+        std = np.std(X_train[column])
+        X_train[column] = (X_train[column] - u) / std
+        return X_train
+
+    X = normalize(X, "Angle")
+    X = normalize(X, "Distance")
+
+    columns = X.columns
+
+    X = X.to_numpy()
+
+    lr = MultinomialLogReg()
+    lr.build(X, y)
+
+    df_B = pd.DataFrame(np.concatenate((lr.B, [[p] for p in possible_outcomes]), axis = 1), columns = np.concatenate((columns, ["B_0", "possible_outcomes"])))
+    df_B = df_B.set_index("possible_outcomes")
+    df_B.to_csv("part_2_betas.csv")
+    print("Saved B to file")
+
+    print("Part 3")
+    rand = random.Random(0)
+    X, y = multinomial_bad_ordinal_good(MBOG_TRAIN + 1000, rand)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, shuffle = True, test_size=1000, random_state=42)
+
+    lr = MultinomialLogReg()
+    lr.build(X_train, y_train)
+    prob = lr.predict(X_test)
+    y_pred = [max(enumerate(p),key=lambda x: x[1])[0] for p in prob]
+
+    possible_outcomes = np.unique(y_train)
+    for i, possible_outcome in zip(range(len(possible_outcomes)), possible_outcomes):
+        y_pred = [possible_outcome if j==i else j for j in y_pred]
+
+    print("Log score Multinomial", lr.best_log_score)
+
+    lr = OrdinalLogReg()
+    lr.build(X_train, y_train)
+    prob = lr.predict(X_test)
+    y_pred = [max(enumerate(p),key=lambda x: x[1])[0] for p in prob]
+
+    for i, possible_outcome in zip(range(len(possible_outcomes)), possible_outcomes):
+        y_pred = [possible_outcome if j==i else j for j in y_pred]
+
+    print("Log score Ordinal", lr.best_log_score)
+
